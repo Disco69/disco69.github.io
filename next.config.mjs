c39dd31530ee0c4b6a8e1faaa5ccb3709f87cb9c
noomeric_is_me <disco69@gmail.com>
2025-07-08 06:11:28 +0700

    Update repository name to planner69

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/personal-finance-planner',
+  basePath: '/planner69',
 };
 
 export default nextConfig;
